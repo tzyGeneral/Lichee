68185144b15b3394ec236e4f9cbbe2403d670fd1
zhenyuan <936451076@qq.com>
2019-08-26 12:50:14 +0800

    测试方法的可行性

---
+++ demo.py
@@ -0,0 +1,10 @@
+from tools import Tools
+
+
+print(Tools.get_rankListDic([{'1':1},{'2':2},{'4':4},{'3':3}]))
+
+print(Tools.get_cssEncryption_str("<div>&#xFF08;2015&#xFF09;</div>"))
+
+print(Tools.get_cleanHtml_str('<div>（2015）</div>'))
+
+print(Tools.dateStrTodatetime("2019-08-26"))
