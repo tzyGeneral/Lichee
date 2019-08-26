6d4fdefcb4fb7ae9d3a2c7bbf2ab8e0c6b30e705
zhenyuan <936451076@qq.com>
2019-08-26 13:13:02 +0800

    测试

--- demo.py
+++ demo.py
@@ -10,3 +10,5 @@ print(Tools.get_cleanHtml_str('<div>（2015）</div>'))
 print(Tools.dateStrTodatetime("2019-08-26"))
 
 print(Tools.getMaxDic({'d1': 2,'d2': 4,'d3':3},reverse=False))
+
+print(Tools.date2Zodiac(5,12))
