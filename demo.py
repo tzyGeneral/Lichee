0cfa2e15b0b7e69a22a97f1b72e3d68639df47cd
zhenyuan <936451076@qq.com>
2019-08-26 13:03:32 +0800

    测试

--- demo.py
+++ demo.py
@@ -8,3 +8,5 @@ print(Tools.get_cssEncryption_str("<div>&#xFF08;2015&#xFF09;</div>"))
 print(Tools.get_cleanHtml_str('<div>（2015）</div>'))
 
 print(Tools.dateStrTodatetime("2019-08-26"))
+
+print(Tools.getMaxDic({'d1': 2,'d2': 4,'d3':3},reverse=False))
